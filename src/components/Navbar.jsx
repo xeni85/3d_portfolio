221117024ebe30fcf673f216e94e5427e8ec842f
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-05-26 18:15:31 -0400

    working on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav 
+      className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 bg-primary`}
+      > 
+        <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+          <Link to='/'></Link>
+        </div>
+      </nav>
   )
 }
 
