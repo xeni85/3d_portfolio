427c6b0914d001ec835cbee74387bfe560cc19a5
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-06-03 13:25:55 -0400

    saturday work on Hero, finally done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,7 @@
 import React, {Suspense, useEffect, useState} from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-// import CanvasLoader from '../Loader'
+import CanvasLoader  from '../CanvasLoader'
 
 const Computers = ({isMobile}) => {
   const computer = useGLTF('../../public/desktop_pc/scene.gltf')
@@ -20,14 +20,28 @@ const Computers = ({isMobile}) => {
       />
       <primitive 
         object={computer.scene}
-        scale={0.75}
-        position={[0, -4.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -2.55, -1.5]}
       />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setMobile] = useState(false)
+
+  useEffect(()=> {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    const handleMediaQueryChange = (event) => {
+      setMobile(event.matches)
+    }
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  },[])
+
   return (
     <Canvas
       frameloop="demand"
@@ -35,13 +49,13 @@ const ComputersCanvas = () => {
       camera={{position: [20, 3, 5], fov: 25}}
       gl={{preserveDrawingBuffer: true}}
     >
-      <Suspense /*fallback={<CanvasLoader />}*/>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false} 
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile = {isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
