c083936dc5a3719e7dc609db254ed197d270b114
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-06-04 17:40:39 -0400

    HOC created, About completed, started working on Experience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,8 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+import { SectionWrapper } from '../hoc'
+
 const ServiceCard = ({index, title, icon}) => {
   return (
     <Tilt className='xs:w-[250px] w-full'>
@@ -22,6 +24,7 @@ const ServiceCard = ({index, title, icon}) => {
             className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
           >
             <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+            <h3 className='text-white text-[2-px] font-bold text-center'>{title}</h3>
           </div>
         </motion.div>
     </Tilt>
@@ -50,4 +53,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
