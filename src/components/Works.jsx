5e588b34c59bdfa356941791e66ff02c6d80c027
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-08-08 15:27:52 -0400

    Started working on it again

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Tilt from 'react-parallax-tilt';
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles';
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { textVariant } from '../utils/motion';
+
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div
+        variants={textVariant()}
+      >
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+    </>
   )
 }
 
